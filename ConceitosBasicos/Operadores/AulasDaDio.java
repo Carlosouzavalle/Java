5eaae43211ce38f96dc81dd213dfe6c06c06f3dc
carlosouzavalle <carlosouzavalle@gmail.com>
2026-09-13 19:49:23 -0300

    Algumas aulinhas a mais do bootcamp da DIO

---
+++ ConceitosBasicos/Operadores/AulasDaDio.java
@@ -0,0 +1,13 @@
+package Operadores;
+import java.util.Scanner;
+
+
+public class AulasDaDio {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("Quanto é 2 + 2?");
+        var result = scanner.nextInt();
+        var isWrong = result != 4;
+        System.out.printf("O resultado é 4, você acertou? (%s) \n", isWrong);
+    }
+}
