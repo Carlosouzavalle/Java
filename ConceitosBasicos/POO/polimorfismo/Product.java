9396c1afdfb28eb9dcaa477bcdb2c881972dcffc
carlosouzavalle <carlosouzavalle@gmail.com>
2026-09-17 15:49:20 -0300

    erroPolimorfismo

---
+++ ConceitosBasicos/POO/polimorfismo/Product.java
@@ -0,0 +1,12 @@
+package POO.polimorfismo;
+
+public class Product {
+
+    public String nome;
+    public double preco;
+
+    public Product(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
---
+++ ConceitosBasicos/POO/polimorfismo/listProducts.java
@@ -0,0 +1,12 @@
+package POO.polimorfismo;
+
+import java.util.ArrayList;
+
+public class listProducts {
+    public static void main(String[] args) {
+        ArrayList<Product> arrayListProducts = new ArrayList<>();
+
+        arrayListProducts.add(new Product("Notebook ASUS VIVO BOOk", 4500));
+
+    }
+}
